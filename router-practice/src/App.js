a18c11d8f5dc240d17598698f4d94498d814d7da
hjjyhj <95295245+hjjyhj@users.noreply.github.com>
2023-07-04 20:15:27 -0400

    router practice

---
+++ router-practice/src/App.js
@@ -0,0 +1,33 @@
+import { Link, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { BookList } from "./pages/BookList";
+import { Book } from "./pages/Book";
+import { NewBook } from "./pages/NewBook";
+import { NotFound } from "./pages/NotFound";
+function App() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/books">Books</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books">
+          <Route index element={<BookList />} />
+          <Route path=":id" element={<Book/>} />
+          <Route path="new" element={<NewBook/>} />
+        </Route>
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ router-practice/src/pages/BookList.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+export function BookList() {
+    return (
+        <>
+            <h1>BookList</h1>
+            <Link to="/book/1">Book 1</Link>
+            <br />
+            <Link to="/book/2">Book 2</Link>
+            <br />
+            <Link to="/book/2">New Book</Link>
+        </>
+    )
+}
